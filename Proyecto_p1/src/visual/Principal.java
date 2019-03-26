b4c721c505b04f463b4018841224e03b0097d12c
Anna <Anna@LAPTOP-38G3TG7K>
2019-03-26 15:27:16 -0600

    agregue boton

--- Proyecto_p1/src/visual/Principal.java
+++ Proyecto_p1/src/visual/Principal.java
@@ -12,6 +12,10 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Toolkit;
 import javax.swing.ImageIcon;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import java.awt.Font;
 
 public class Principal extends JFrame {
 
@@ -44,24 +48,54 @@ public class Principal extends JFrame {
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+		setContentPane(contentPane);;
+		
+
+		JPanel panel_1 = new JPanel();
+		contentPane.add(panel_1, BorderLayout.CENTER);
+		panel_1.setLayout(null);
+		
+		JMenuBar menuBar = new JMenuBar();
+		menuBar.setBounds(0, 0, 1340, 21);
+		panel_1.add(menuBar);
+		
+		JMenu mnEquipo = new JMenu("Equipo");
+		menuBar.add(mnEquipo);
+		
+		JMenuItem mntmRegistrar = new JMenuItem("Registrar");
+		mnEquipo.add(mntmRegistrar);
+		
+		JMenuItem mntmLista = new JMenuItem("Lista");
+		mnEquipo.add(mntmLista);
+		
+		JMenu mnCalendario = new JMenu("Calendario");
+		menuBar.add(mnCalendario);
+		
+		JMenuItem mntmVerCalendario = new JMenuItem("Ver Calendario");
+		mnCalendario.add(mntmVerCalendario);
+		
+		JMenu mnEstadisticas = new JMenu("Estadisticas");
+		menuBar.add(mnEstadisticas);
+		
+		JMenuItem mntmEstadisticaDelEquipo = new JMenuItem("Estadistica Del Equipo");
+		mnEstadisticas.add(mntmEstadisticaDelEquipo);
+		
+		JMenuItem mntmEstadisticaDelJugador = new JMenuItem("Estadistica Del Jugador");
+		mnEstadisticas.add(mntmEstadisticaDelJugador);
+		
+		JMenuItem mntmEstadisticaDelPartido = new JMenuItem("Estadistica Del Partido");
+		mnEstadisticas.add(mntmEstadisticaDelPartido);
+		
+		JButton btnJuego = new JButton("Juego");
+		btnJuego.setFont(new Font("Tahoma", Font.PLAIN, 18));
+		btnJuego.setBounds(598, 248, 184, 167);
+		panel_1.add(btnJuego);
 		dimention = super.getToolkit().getScreenSize();
 		super.setSize(dimention.width,(dimention.height-50));
 		setLocationRelativeTo(null);
 		
-		JPanel panel = new JPanel();
-		contentPane.add(panel, BorderLayout.CENTER);
-		panel.setLayout(new BorderLayout(0, 0));
 		
-		JButton btnNba = new JButton();
-		btnNba.setIcon(new ImageIcon(Principal.class.getResource("/imagen/logoprincipal2.png")));
-		btnNba.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-			}
-		});
-		panel.add(btnNba, BorderLayout.CENTER);
 		dimention=super.getToolkit().getScreenSize();
 		super.setSize(dimention.width,(dimention.height-50));
 	}
-
 }
