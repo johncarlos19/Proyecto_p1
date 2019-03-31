d5abb2164a6ed17ac8a998a98b1a85656240ecea
John Lopez <John Lopez@MSI>
2019-03-31 14:54:21 -0400

    agregue una clase Imgtabla para poder agregar foto a las tabla

---
+++ Proyecto_p1/src/visual/ImgTabla.java
@@ -0,0 +1,24 @@
+package visual;
+
+import java.awt.Component;
+
+import javax.swing.JLabel;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableCellRenderer;
+
+
+public class ImgTabla extends DefaultTableCellRenderer {
+
+	@Override
+	public Component getTableCellRendererComponent(JTable jtable, Object o, boolean bln, boolean bln1, int i, int i1) {
+		if (o instanceof JLabel) {
+			JLabel lbl = (JLabel)o;
+			return lbl;
+		}
+		
+		return super.getTableCellRendererComponent(jtable, o, bln, bln1, i, i1);
+	}
+	
+	
+	
+}
--- Proyecto_p1/src/visual/ListarEquipos.java
+++ Proyecto_p1/src/visual/ListarEquipos.java
@@ -2,15 +2,21 @@ package visual;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.Image;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumnModel;
 
+import javafx.scene.layout.ColumnConstraints;
 import logico.Baloncesto;
 import logico.Equipo;
 
@@ -19,6 +25,7 @@ import javax.swing.JTable;
 import javax.swing.ListSelectionModel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Font;
 
 public class ListarEquipos extends JDialog {
 
@@ -62,27 +69,39 @@ public class ListarEquipos extends JDialog {
 				JScrollPane scrollPane = new JScrollPane();
 				panel.add(scrollPane, BorderLayout.CENTER);
 				{
-					String[] header = {"Nombre", "Coach", "Cancha"};
-					model = new DefaultTableModel();
-					model.setColumnIdentifiers(header);
 					
+					//model.setColumnIdentifiers(header);
+					//model.set
 					table = new JTable();
+					
+					
+					table.setFont(new Font("Tahoma", Font.PLAIN, 18));
+					table.setDefaultRenderer(Object.class, new ImgTabla());
+					String[] header = {"Logo","Nombre", "Coach", "Cancha"};
+					//Icon icono11 = new ImageIcon(Baloncesto.getInstance().getMisEquipos().get(0).getLogo().getImage().getScaledInstance(32, 32, Image.SCALE_DEFAULT));
+					
+					model = new DefaultTableModel(null,header);
 					table.addMouseListener(new MouseAdapter() {
 						@Override
 						public void mouseClicked(MouseEvent e) {
 							if(table.getSelectedRow()>=0){
 								btnVer.setEnabled(true);
 								int index = table.getSelectedRow();
-								identificador = (String)table.getModel().getValueAt(index, 0);
+								identificador = (String)table.getModel().getValueAt(index, 1);
 				
 							}
 						}
 					});
 					
 					table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+					
+					//table.setValueAt(icono11, 1, 1);
+					table.setRowHeight(40);
 					table.setModel(model);
+					table.setOpaque(false);
 					cargarTabla();
 					scrollPane.setViewportView(table);
+					scrollPane.setOpaque(false);
 				}
 			}
 		}
@@ -134,11 +153,17 @@ public class ListarEquipos extends JDialog {
 		model.setRowCount(0);
 		fila = new Object[model.getColumnCount()];
 		for (int i = 0; i < Baloncesto.getInstance().getMisEquipos().size(); i++) {
-			fila[0] = Baloncesto.getInstance().getMisEquipos().get(i).getNombre();
-			fila[1] = Baloncesto.getInstance().getMisEquipos().get(i).getCoach();
-			fila[2] = Baloncesto.getInstance().getMisEquipos().get(i).getCancha();
+			//
+					//.getImage().getScaledInstance(ColumnConst, lblLogoequipo11.getHeight(), Image.SCALE_DEFAULT));
+			Icon icono111 = new ImageIcon(Baloncesto.getInstance().getMisEquipos().get(i).getLogo().getImage().getScaledInstance(40, 40, Image.SCALE_DEFAULT));
+			//JLabel log = ;
+			fila[0]=new JLabel(icono111);
+			fila[1] = Baloncesto.getInstance().getMisEquipos().get(i).getNombre();
+			fila[2] = Baloncesto.getInstance().getMisEquipos().get(i).getCoach();
+			fila[3] = Baloncesto.getInstance().getMisEquipos().get(i).getCancha();
 			model.addRow(fila);
 		}
+		
 		
 	}
 }
